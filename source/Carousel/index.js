c3594920b821217bd9bb09d54d4aeea09040e40a
Samuel William Reinhardt <sam@bytes.co>
2023-06-18 16:20:39 -0400

    Fix DOM define custom elems, apply start of styling

--- source/Carousel/index.js
+++ source/Carousel/index.js
@@ -1,17 +1,53 @@
 import { LitElement, html, css } from 'lit-element'
 
 export class Carousel extends LitElement {
+	static properties = {
+		"show-heading": { type: Boolean, attribute: true }, // visibly show the carousel heading
+		"vertical": { type: Boolean, attribute: true } // 
+	}
+
+	constructor() {
+		super()
+	}
+
 	render() {
 		return html`
-			<section>
-				<slot name="interface-top" />
-				<div>
-					<slot name="slides" />
-				</div>
-				<slot name="interface-left" />
-				<slot name="interface-right" />
-				<slot name="interface-bottom" />
+			<section aria-labelledby="carousel-heading">
+				<slot id="carousel-heading" name="heading" ?hidden=${!this['show-heading']} ></slot>
+				<slot name="interface-top"></slot>
+				<slot name="slides"></slot>
+				<slot name="interface-left"></slot>
+				<slot name="interface-right"></slot>
+				<slot name="interface-bottom"></slot>
 			</section>
 		`
 	}
+
+	static styles = [
+		css`
+			:host {
+				display: grid;
+				grid-template-areas:
+					"Heading Heading Heading"
+					". Top ."
+					"Left Slides Right"
+					". Bottom .";
+			}
+			slot['name="heading"] {
+				grid-area: Heading;
+			}
+			slot[name="interface-top"] {
+				grid-area: Top;
+			}
+			slot[name="interface-right"] {
+				grid-area: Right;
+			}
+			slot[name="interface-bottom"] {
+				grid-area: Bottom;
+			}
+			slot[name="interface-left"] {
+				grid-area: Left;
+			}
+		`
+	]
 }
--- source/index.js
+++ source/index.js
@@ -1,7 +1,7 @@
-import { Carousel } as VectorCarousel from './Carousel/index.js'
-import { Slide } as VectorCarouselSlide from 'Slide/index.js'
-import { Control } as VectorCarouselControl from './Control/index.js'
-import { Nav } as VectorCarouselNav from './Nav/index.js'
+import { Carousel as VectorCarousel } from './Carousel/index.js'
+import { Slide as VectorCarouselSlide } from './Slide/index.js'
+import { Control as VectorCarouselControl } from './Control/index.js'
+import { Nav as VectorCarouselNav } from './Nav/index.js'
 
 
 export {
@@ -10,3 +10,8 @@ export {
 	VectorCarouselControl,
 	VectorCarouselNav
 }
+
+customElements.define( 'vector-carousel', VectorCarousel )
+customElements.define( 'vector-carousel-slide', VectorCarouselSlide )
+customElements.define( 'vector-carousel-control', VectorCarouselControl )
+customElements.define( 'vector-carousel-nav', VectorCarouselNav )
