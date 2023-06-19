4408f7c9af746249c3e0af6bf28aa137aa6da244
Samuel William Reinhardt <sam@bytes.co>
2023-06-19 08:08:01 -0400

    Add support for slide visible amount

--- source/Carousel/index.js
+++ source/Carousel/index.js
@@ -1,25 +1,64 @@
-import { LitElement, html, css } from 'lit-element'
+import { LitElement, html, css, unsafeCSS } from 'lit-element'
 
 export class Carousel extends LitElement {
 	static properties = {
-		"visible-heading": { type: Boolean, attribute: true }, // visibly show the carousel heading
-		"vertical": { type: Boolean, attribute: true } // 
+		show_heading: { type: Boolean, attribute: true }, // visibly show the carousel heading
+		vertical: { type: Boolean, attribute: true },
+		starting_slide: { type: Number, attribute: true },
+		show_overflow: { type: Boolean, attribute: true },
+		slide_visible_amount: { type: Number, attribute: true },
+		slide_gap: { type: css, attribute: true },
+		slide_scroll_amount: { type: Number, attribute: true },
+		_active_slide: { type: Number, state: true }
 	}
 
 	constructor() {
 		super()
+
+		this.starting_slide = 0
+		this.slide_visible_amount = 1
+		this.slide_gap = "0px"
 	}
 
-	render() {
+	addDynamicStyles() {
+		
+	}
+
+	renderHeading() {
+		const hidden = this.show_heading ? '' : 'hidden'
 		return html`
+			<slot id="carousel-heading" name="heading" .hidden=${hidden} ></slot>
+		`
+	}
+
+	render() { 
+		return html`
+			${this.renderDynamicStyles()}
 			<section class="carousel" aria-labelledby="carousel-heading">
-				<slot id="carousel-heading" name="heading" ?hidden=${!this['visible-heading']} ></slot>
+				${this.renderHeading()}
 				<slot name="interface-top"></slot>
 				<slot name="slides"></slot>
 				<slot name="interface-left"></slot>
 				<slot name="interface-right"></slot>
 				<slot name="interface-bottom"></slot>
 			</section>
+		`
+	}
+
+	renderDynamicStyles() {
+		const slide_size = 100 * ( 1 / this.slide_visible_amount )
+		const gap_count = ( this.slide_visible_amount - 1 ) | 0
+		const slide_gap_calc = css`
+			${unsafeCSS(this.slide_gap)} / ${this.slide_visible_amount} * ${gap_count}` 
+		
+		const styles = css`
+			:host {
+				--slide-gap: ${unsafeCSS(this.slide_gap)};
+				--slide-size: calc( ${slide_size}% - ${slide_gap_calc} );
+			}
+		`
+		return html`
+			<style>${styles}</style>
 		`
 	}
 
@@ -32,9 +71,27 @@ export class Carousel extends LitElement {
 					". Top ."
 					"Left Slides Right"
 					". Bottom .";
+					grid-template-columns: min-content 1fr min-content;
+					grid-template-rows: min-content min-content 1fr min-content;
 			}
-			slot['name="heading"] {
+			slot {
+				display: flex;
+			}
+			slot[hidden] {
+				display: none;
+			}
+			slot[name="heading"] {
 				grid-area: Heading;
+			}
+			slot[name="slides"] {
+				grid-area: Slides;
+				flex-wrap: nowrap;
+				gap: var(--slide-gap, 0px);
+			
+				overflow: visible;
+			}
+			slot[name="slides"]::slotted(vector-carousel-slide) {
+				flex: 1 0 var(--slide-size, 100%);
 			}
 			slot[name="interface-top"] {
 				grid-area: Top;
